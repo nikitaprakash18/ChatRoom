948b3ffc2a93e68f62ada634050b0d37f1ec0a06
NikitaPrakash Patil <nikitaprakashpatil@NikitaPrakashs-MacBook-Pro.local>
2018-08-25 22:18:52 -0700

    first commit

---
+++ Chat/AppController.swift
@@ -0,0 +1,70 @@
+//
+//  AppController.swift
+//  Chat
+//
+//  Created by NikitaPrakash Patil on 8/24/18.
+//  Copyright © 2018 NikitaPrakash Patil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+final class AppController {
+    
+    static let shared = AppController()
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(userStateDidChange),
+            name: Notification.Name.AuthStateDidChange,
+            object: nil
+        )
+    }
+    
+    private var window: UIWindow!
+    private var rootViewController: UIViewController? {
+        didSet {
+            if let vc = rootViewController {
+                window.rootViewController = vc
+            }
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    func show(in window: UIWindow?) {
+        guard let window = window else {
+            fatalError("Cannot layout app with a nil window.")
+        }
+        
+        FirebaseApp.configure()
+        
+        self.window = window
+        window.tintColor = .primary
+        window.backgroundColor = .white
+        
+        handleAppState()
+        
+        window.makeKeyAndVisible()
+    }
+    
+    private func handleAppState() {
+        if let user = Auth.auth().currentUser {
+            let vc = ProfileViewController(currentUser: user)
+            rootViewController = NavigationController(vc)
+        } else {
+            rootViewController = LoginViewController()
+        }
+    }
+    
+    // MARK: - Notifications
+    
+    @objc internal func userStateDidChange() {
+        DispatchQueue.main.async {
+            self.handleAppState()
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, "11.0"
+
+target 'Chat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'MessageKit'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'FirebaseStorage'
+  pod 'Firebase/Firestore'
+  post_install do |installer|
+      installer.pods_project.targets.each do |target|
+          if target.name == 'MessageKit'
+              target.build_configurations.each do |config|
+                  config.build_settings['SWIFT_VERSION'] = '4.0'
+              end
+          end
+      end
+  end
+end
